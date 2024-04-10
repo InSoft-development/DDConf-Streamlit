f846f76522fd4696d80733cc450c6f9487e908e3
RVM <rvmezin@inctrl.ru>
2024-04-10 13:47:20 +0300

    d104: added text describing the checkboxes (fix3)

--- pages/dd104multi.py
+++ pages/dd104multi.py
@@ -887,9 +887,9 @@ def _ld_create_form(loadout:dict, box:st.empty):
 			cont = st.container()
 			# sbtn, smsg = st.columns([0.3,0.7])
 			# Statusbox = smsg.empty()
-			col1, col2 = cont.columns([0.8, 0.2])
-			col2.markdown(":bold[Добавить выбор архивных файлов]")
-			col1.markdown(":bold[Назначение процессов]")
+			# col1, col2 = cont.columns([0.8, 0.2])
+			# col2.markdown(":bold[Добавить выбор архивных файлов]")
+			# col1.markdown(":bold[Назначение процессов]")
 			if loadout['fcount'] <= 0:
 				with cont:
 					
@@ -903,7 +903,7 @@ def _ld_create_form(loadout:dict, box:st.empty):
 					
 					col1, col2 = st.columns([0.8, 0.2])
 					
-					col2.checkbox(label="Использовать файлы из архива", value=st.session_state.dd104m['ld-archive-use-flag'][0] if 0 in st.session_state.dd104m['ld-archive-use-flag'].keys() else False, on_change=checker, kwargs={'i':0}, label_visibility="collapsed", key=f'ld-archive-use-cbox-0')
+					col2.checkbox(label="Добавить выбор архивных файлов", value=st.session_state.dd104m['ld-archive-use-flag'][0] if 0 in st.session_state.dd104m['ld-archive-use-flag'].keys() else False, on_change=checker, kwargs={'i':0}, key=f'ld-archive-use-cbox-0')
 					
 					col1.selectbox(label=f'Файл настроек процесса 1', options=(files if (0 in st.session_state.dd104m[f'ld-archive-use-flag'].keys() and not st.session_state.dd104m[f'ld-archive-use-flag'][0] or not 0 in st.session_state.dd104m[f'ld-archive-use-flag'].keys()) else files+arch_files), index=None, on_change=validate, key=f"select_file_0")
 						
